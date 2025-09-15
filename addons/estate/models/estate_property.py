6dd55b5ef8fd8796cfbde88bc4a4d0bbc451ab9e
Jannespph <jannespakpahan645@gmail..com>
2025-09-15 20:49:24 +0700

    Chapter 5: Finally, Some UI To Play With

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -1,17 +1,18 @@
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta    
 
 class EstateProperty(models.Model):
-    _name = "estate.property"
-    _description = "Estate Property"   #<--- ini adalah private atribut yang digunakan sebagai identitas dari class
+    _name = "estate.property"           #|
+    _description = "Estate Property"    #|  <--- ini adalah private atribut yang digunakan sebagai identitas dari class
 
     name = fields.Char(required=True)    #<------Di Odoo semua field huruf pertamanya kapital,kalau tidak akan terjadi error
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy = False, default = lambda self: fields.Datetime.now() + relativedelta(months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
-    living_are = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
@@ -23,5 +24,18 @@ class EstateProperty(models.Model):
             ('east', 'East'),
             ('west', 'West')
         ]
+    )
+    active = fields.Boolean(default=False) #<------ gunanya field active di Odoo adalah untuk mengontrol apakah data ditampilkan (aktif) atau disembunyikan (diarsipkan) di UI
+    #New, Offer Received, Offer Accepted, Sold and Cancelled.
+    state = fields.Selection(
+        selection=[
+            ('new','New'),  #<----- catatn yang sebelah kiri itu value untuk database kalok yang kanan untuk di tampilkan di UI
+            ('offer_received','Offer Received'),
+            ('offer_accepted','Offer Accepted'),
+            ('cancelled','Cancelled'),
+        ],
+        required = True,
+        copy = False,
+        default ='new',
     )
 
